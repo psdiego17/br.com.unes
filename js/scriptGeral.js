7216f550f16c9d17c97a11b499cdfdd961c939b3
Diêgo Souza <psdiego17@gmail.com>
2021-07-25 19:23:09 -0300

    Criação da página unes, estudo de desenvolvimento web

---
+++ js/scriptGeral.js
@@ -0,0 +1,9 @@
+/*
+function myFunction() {
+    var x = document.getElementById('navbarNav');
+    if (x.style.visibility === 'hidden') {
+      x.style.visibility = 'visible';
+    } else {
+      x.style.visibility = 'hidden';
+    }
+}*/
